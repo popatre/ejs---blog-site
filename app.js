7fc21e0c4e42c5270bb9df484b3439fa05309077
popatre <76402245+popatre@users.noreply.github.com>
2021-11-20 17:07:39 +0000

    adds to database

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
 const _ = require("lodash");
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/blogDB", { useNewUrlParser: true });
+
+// schema************
+const postSchema = { title: String, content: String };
+
+//model****************************
+const Post = mongoose.model("Post", postSchema);
 
 const homeStartingContent =
     "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -9,15 +19,16 @@ const aboutContent =
 const contactContent =
     "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const app = express();
-const posts = [];
+// const posts = [];
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res, next) => {
-    res.render("home", { homeStartingContent, posts });
+    Post.find({}, (err, posts) => {
+        res.render("home", { homeStartingContent, posts: posts });
+    });
 });
 app.get("/about", (req, res, next) => {
     res.render("about", { aboutContent });
@@ -31,18 +42,22 @@ app.get("/compose", (req, res, next) => {
 
 app.post("/compose", (req, res, next) => {
     const { postContent, postTitle } = req.body;
-    const post = { title: postTitle, body: postContent };
-    posts.push(post);
-    res.redirect("/");
+
+    const post = new Post({ title: postTitle, content: postContent });
+    post.save((err) => {
+        if (!err) {
+            res.redirect("/");
+        }
+    });
+
+    // const post = { title: postTitle, body: postContent };
+    // posts.push(post);
 });
 
 app.get("/posts/:post", (req, res, next) => {
-    const param = _.lowerCase(req.params.post);
-
-    posts.forEach((a) => {
-        if (_.lowerCase(a.title) === param) {
-            res.render("post", { title: a.title, body: a.body });
-        }
+    const param = req.params.post;
+    Post.findById(param, (err, posts) => {
+        res.render("post", { title: posts.title, body: posts.content });
     });
 });
 
